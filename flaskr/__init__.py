5124737b3f8db0e2663b6e5a1c4ab1f64271421b
NullDude05 <jdandiaz2021@gmail.com>
2023-09-21 17:32:09 -0500

    updated flask as an app

---
+++ flaskr/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask, render_template
+
+def create_app(test_config=None):
+    #create and configure app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # Load the instance config, if exists when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    #ensure instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    #index
+    @app.route('/')
+    def index():
+        return render_template("/childtemplates/homepage.html", title="Home")
+    
+    return app
